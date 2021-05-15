0b6eaed0abc22e1965b277f3634f2b606831325e
DenysVetriak <44784005+DenysVetriak@users.noreply.github.com>
2021-05-15 21:12:23 +0300

    Update file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,3 @@
-#!/bin/sh
-
-# Replace the hostname in the container
-sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /usr/share/nginx/html/index.html
-
-# Startup the cmd
-exec "$@"
-
-
-
-
-
-
 
 #!/bin/bash
 #MY_NAME=Max
